d4a889d6976ece60cbf6ae0737045751ee2ec9fb
Mike Bernal <mikebernal.me@gmail.com>
2021-01-13 11:24:23 +0800

    feat: add staticProps and staticPaths

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,11 +1,41 @@
 import Layout from '../../components/Layout/Layout';
 import styles from './Country.module.css';
 
-export default function Country() {
+async function getCountry(id) {
+    const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
+    const country = await res.json();
+    return country;
+}
+
+export default function Country({ country }) {
 
     return (
-        <Layout>
+        <Layout title={country.name}>
 
         </Layout>
     )
+}
+
+export async function getStaticPaths() {
+    const res = await fetch(`https://restcountries.eu/rest/v2/all`);
+    const countries = await res.json();
+
+    const paths = countries.map((country) => ({
+        params: { id: country.alpha3Code }
+    }));
+
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const country = await getCountry(params.id);
+
+    return {
+        props: {
+            country,
+        }
+    }
 }
