58fb9037757cc2b7f7e1c02128c37aafadf0616f
Mike Bernal <mikebernal.me@gmail.com>
2021-01-13 08:33:12 +0800

    feat: add search ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,20 @@ import Layout from '../components/Layout/Layout';
 import CountriesTable from '../components/CountriesTable/CountriesTable';
 
 import styles from '../styles/Home.module.css'
+import SearchInput from '../components/SearchInput/SearchInput';
 
 export default function Home({ countries }) {
   console.log(countries);
   return (
     <Layout>
-      <div className={styles.search}>
+      <div className={styles.input_container}>
         {/* Country counts */}
-        <div className={styles.search_counts}>Found {countries.length} countries</div>
+        <div className={styles.counts}>Found {countries.length} countries</div>
 
         {/* Search input */}
-        <div className={styles.search_input}>Search Input</div>
+        <div className={styles.input}>
+          <SearchInput placeholder="Search by country name, region, or subregion" />
+        </div>
       </div>
 
       {/* Countries table component*/}
